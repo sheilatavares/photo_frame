d962dc4554c6480aa3c007532d7a662fe7d9ed12
Sheila Tavares <sheilatavares@MacBook-Pro-de-Sheila.local>
2024-01-31 18:40:07 -0300

    crop library

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,41 +1,32 @@
-import { useRef, useState } from "react";
-import PencilIcon from "./PencilIcon";
-import Modal from "./Modal";
+import React, { useRef, useEffect, useState } from "react";
+import { openEditor } from "https://esm.sh/react-profile";
+
+import ReactDOM from "https://esm.sh/react-dom";
 
 const Profile = () => {
-  const avatarUrl = useRef(
-    "https://avatarfiles.alphacoders.com/161/161002.jpg"
-  );
-  const [modalOpen, setModalOpen] = useState(false);
+  const [dataURL, setDataURL] = useState();
 
-  const updateAvatar = (imgSrc) => {
-    avatarUrl.current = imgSrc;
+  const open = async (e) => {
+    const res = await openEditor(e.target.files[0]);
+    const image = res.editedImage;
+    if (image) {
+      const url = image.getDataURL();
+      setDataURL(url);
+    }
   };
 
   return (
-    <div className="flex flex-col items-center pt-12">
-      <div className="relative">
-        <img
-          src={avatarUrl.current}
-          alt="Avatar"
-          className="w-[150px] h-[150px] rounded-full border-2 border-gray-400"
-        />
-        <button
-          className="absolute -bottom-3 left-0 right-0 m-auto w-fit p-[.35rem] rounded-full bg-gray-800 hover:bg-gray-700 border border-gray-600"
-          title="Change photo"
-          onClick={() => setModalOpen(true)}
-        >
-          <PencilIcon />
-        </button>
-      </div>
-      <h2 className="text-white font-bold mt-6">Mack Aroney</h2>
-      <p className="text-gray-500 text-xs mt-2">Software Engineer</p>
-      {modalOpen && (
-        <Modal
-          updateAvatar={updateAvatar}
-          closeModal={() => setModalOpen(false)}
-        />
-      )}
+    <div>
+      <label for="file">Please select your image</label> <br />
+      <br />
+      <input
+        type="file"
+        id="file"
+        name="file"
+        accept="image/jpeg;image/png"
+        onChange={open}
+      />
+      {dataURL && <img src={dataURL} alt="Preview image" />}
     </div>
   );
 };
